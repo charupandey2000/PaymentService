0cdad85ccd28573dc888c80a1eb43a7d63fddddf
charupandey2000 <charupandey2000@gmail.com>
2024-06-09 00:01:50 +0530

    create event

--- src/main/java/dev/charu/paymentservice/Controller/Webhook/WebHookController.java
+++ src/main/java/dev/charu/paymentservice/Controller/Webhook/WebHookController.java
@@ -1,15 +1,19 @@
 package dev.charu.paymentservice.Controller.Webhook;
 
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.model.Event;
 import com.stripe.net.Webhook;
 import org.springframework.web.bind.annotation.*;
-import org.w3c.dom.events.Event;
 
 @RestController
 @RequestMapping("/stripeWebhook")
 public class WebHookController {
     @PostMapping
-    public void HandleWebHookRequest(@RequestBody Webhook webhooks){
-         return;
+    public void HandleWebHookRequest( @RequestBody String payload,@RequestHeader("Stripe-Signature") String sigHeader) throws SignatureVerificationException {
+        Event event;
+        String sec="whsec_fOxRlOAYF4dJKdstRJ8aZfRnLZHieDiL";
+        event=Webhook.constructEvent(payload,sigHeader,sec);
+        return;
     }
 
 }
